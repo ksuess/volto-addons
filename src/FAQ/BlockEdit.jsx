32bdfc1f1583b47facb1fc7c4f4902bf412ec4f6
Katja Suess <k.suess@rohberg.ch>
2020-08-17 21:00:45 +0200

    Update BlockEdit.jsx

--- src/FAQ/BlockEdit.jsx
+++ src/FAQ/BlockEdit.jsx
@@ -1,14 +1,13 @@
 import React from 'react';
 import { SidebarPortal } from '@plone/volto/components';
 import FAQSidebar from './FAQSidebar';
-import FAQBody from './FAQBody';
 
 const Edit = ({ data, onChangeBlock, block, selected, properties }) => {
   return (
     <div
       className={cx('block faq')}
     >
-      <FAQBody data={data} properties={properties} id={block} isEditMode />
+      <div>TODO FAQBody</div>
       <SidebarPortal selected={selected}>
         <FAQSidebar data={data} block={block} onChangeBlock={onChangeBlock} />
       </SidebarPortal>
