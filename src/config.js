94426915bc957ec6c58ca7ef686f00bb1c240952
Katja Suess <k.suess@rohberg.ch>
2020-08-19 11:06:16 +0200

    Update config.js

--- src/config.js
+++ src/config.js
@@ -13,6 +13,13 @@ export default function applyConfig(config) {
     view: FAQBlockView,
     icon: chartIcon,
     group: 'common',
+    restricted: false,
+    mostUsed: true,
+    sidebarTab: 1,
+    security: {
+      addPermission: [],
+      view: [],
+    },
   };
   
   return config;
