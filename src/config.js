ee0ec7a20d24a3b7158d6c40299a9915a71a624c
Katja Suess <k.suess@rohberg.ch>
2020-08-19 11:01:34 +0200

    default export!: export default function applyConfig(config) {

--- src/config.js
+++ src/config.js
@@ -1,19 +1,22 @@
 import chartIcon from '@plone/volto/icons/world.svg';
 
-import {View as FAQBlockView} from './FAQ/BlockView';
-import {Edit as FAQBlockEdit} from './FAQ/BlockEdit';
+import FAQBlockEdit from './FAQ/BlockEdit';
+import FAQBlockView from './FAQ/BlockView';
 
 
-export function applyConfig(config) {
+export default function applyConfig(config) {
 
-  // config.blocks.blocksConfig.faq_viewer = {
-  //   id: 'faq_viewer',
-  //   title: 'FAQ Viewer',
-  //   view: FAQBlockView,
-  //   edit: FAQBlockEdit,
-  //   icon: chartIcon,
-  //   group: 'common',
-  // };
+  config.blocks.blocksConfig.faq_viewer = {
+    id: 'faq_viewer',
+    title: 'FAQ Viewer',
+    edit: FAQBlockEdit,
+    view: FAQBlockView,
+    icon: chartIcon,
+    group: 'common',
+  };
   
   return config;
 }
+
+
+// export default config => config;
