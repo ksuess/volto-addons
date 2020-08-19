fcd2af58ef40e5f216216c27e4878f39daaeea3b
Katja Suess <k.suess@rohberg.ch>
2020-08-19 10:35:34 +0200

    alias

--- src/config.js
+++ src/config.js
@@ -1,19 +1,19 @@
 import chartIcon from '@plone/volto/icons/world.svg';
 
-import FAQBlockView from './FAQ/BlockView';
-import FAQBlockEdit from './FAQ/BlockEdit';
+import {View as FAQBlockView} from './FAQ/BlockView';
+import {Edit as FAQBlockEdit} from './FAQ/BlockEdit';
 
 
 export function applyConfig(config) {
 
-  config.blocks.blocksConfig.faq_viewer = {
-    id: 'faq_viewer',
-    title: 'FAQ Viewer',
-    view: FAQBlockView,
-    edit: FAQBlockEdit,
-    icon: chartIcon,
-    group: 'common',
-  };
+  // config.blocks.blocksConfig.faq_viewer = {
+  //   id: 'faq_viewer',
+  //   title: 'FAQ Viewer',
+  //   view: FAQBlockView,
+  //   edit: FAQBlockEdit,
+  //   icon: chartIcon,
+  //   group: 'common',
+  // };
   
   return config;
 }
